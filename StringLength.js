dd10e8d65ce145d0cbc6a703fa28f623c30d9fd3
gtekle <teklegy@gmail.com>
2022-01-17 22:03:33 +0300

    Add test case to check if error is thrown by the stringLength() function when string length is not between 1 and 10 inclusive

--- StringLength.js
+++ StringLength.js
@@ -1,4 +1,6 @@
 const stringLength = (string) => {
+  if (string.length < 1 || string.length > 10)
+    throw new Error('String length is out of range (1 -10)');
   return string.length;
 }
 
--- StringLength.test.js
+++ StringLength.test.js
@@ -3,3 +3,10 @@ const stringLength = require('./StringLength.js');
 it("string length", () => {
   expect(stringLength("test")).toBe(4);
 })
+
+it("string length out of bound", () => {
+  expect(() => stringLength("test test test test"))
+    .toThrow('String length is out of range (1 -10)');
+  expect(() => stringLength(""))
+    .toThrow('String length is out of range (1 -10)');
+})
